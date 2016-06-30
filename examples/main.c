dc939989453e33ebc313ac872338661ef1a494fd
Alexander <a.baygeldin@gmail.com>
2016-06-30 07:59:59 +0300

    Calling function from Forth REPL

--- examples/main.c
+++ examples/main.c
@@ -7,74 +7,21 @@ typedef struct {
   int y;
 } bar;
 
+int xyz(int a, int b);
 int foo(int a, char *b, bar c);
 
-int main()
-{
-/*  // Function definition
-  ffi_cif cif;
-  ffi_type *args[3];
-  void *values[3];
-
-  // Arguments
-  int arg1;
-  char *arg2;
-  struct bar arg3;
-
-  // Return value
-  int res;
-
-  // Custom type definition
-  ffi_type bar_type;
-  ffi_type *bar_type_elements[3];
-
-  bar_type.size = bar_type.alignment = 0;
-  bar_type.elements = bar_type_elements;
-  bar_type.type = FFI_TYPE_STRUCT;
-
-  bar_type_elements[0] = &ffi_type_sint;
-  bar_type_elements[1] = &ffi_type_sint;
-  bar_type_elements[2] = NULL;
-
-  // Getting pointer to a called function
-  char *name = "foo";
-  void *handler = dlopen(NULL, RTLD_LAZY);
-  if (!handler)
-  {
-    fprintf(stderr, "dlopen failed: %s\n", dlerror());
-    exit(EXIT_FAILURE);
-  }
-  void *ptr = dlsym(handler, name);
-  dlclose(handler);
-
-  // Arguments initialization
-  values[0] = &arg1;
-  args[0] = &ffi_type_sint;
-  values[1] = &arg2;
-  args[1] = &ffi_type_pointer;
-  values[2] = &arg3;
-  args[2] = &bar_type;
-
-  // Calling :)
-  if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 3, &ffi_type_sint, args) == FFI_OK)
-    {
-      arg1 = 21;
-      arg2 = "Uh, Oh";
-      arg3.x = 14;
-      arg3.y = 88;
-
-      ffi_call(&cif, ptr, &res, values);
-
-      printf("And the dead beef is %d\n", res);
-    }
-*/
+int main() {
   pfDoForth(NULL, NULL, 0);
   return 0;
 }
 
-int foo(int a, char *b, bar c)
-{
+int foo(int a, char *b, bar c) {
   printf("The integer is %d, string is \"%s\" and struct is  (%d, %d)\n", a, b, c.x, c.y);
   return 0xdeadbeef;
+}
+
+int xyz(int a, int b) {
+  printf("A is %d and B is %d", a, b);
+  return (a + b);
 }
 
--- src/custom.c
+++ src/custom.c
@@ -1,65 +1,48 @@
 #include "../pforth/csrc/pf_all.h"
-/*  // Function definition
-  ffi_cif cif;
-  ffi_type *args[3];
-  void *values[3];
+#include <ffi.h>
+#include <dlfcn.h>
+#include <stdlib.h>
 
-  // Arguments
-  int arg1;
-  char *arg2;
-  struct bar arg3;
+void *handler;
 
-  // Return value
+static cell_t CCall(cell_t count, cell_t func) {
   int res;
+  int i;
+  ffi_cif cif;
+  ffi_type **args = (ffi_type**)malloc(count * sizeof(*args));
+  void **values = (void**)malloc(count * sizeof(void*));
 
-  // Custom type definition
-  ffi_type bar_type;
-  ffi_type *bar_type_elements[3];
+  /* Example with custom structure!
+   * Forth has structures too, so it's a good idea to map them somehow.
+   *
+   * struct bar arg3;
+   * ffi_type bar_type;
+   * ffi_type *bar_type_elements[count];
+   * bar_type.size = bar_type.alignment = 0;
+   * bar_type.elements = bar_type_elements;
+   * bar_type.type = FFI_TYPE_STRUCT;
+   * bar_type_elements[0] = &ffi_type_sint;
+   * bar_type_elements[1] = &ffi_type_sint;
+   * bar_type_elements[2] = NULL;
+   * */
 
-  bar_type.size = bar_type.alignment = 0;
-  bar_type.elements = bar_type_elements;
-  bar_type.type = FFI_TYPE_STRUCT;
-
-  bar_type_elements[0] = &ffi_type_sint;
-  bar_type_elements[1] = &ffi_type_sint;
-  bar_type_elements[2] = NULL;
-
-  // Getting pointer to a called function
-  char *name = "foo";
-  void *handler = dlopen(NULL, RTLD_LAZY);
-  if (!handler)
-  {
-    fprintf(stderr, "dlopen failed: %s\n", dlerror());
-    exit(EXIT_FAILURE);
-  }
+  char name[255];
+  ForthStringToC(name, (char *)func, sizeof(name));
   void *ptr = dlsym(handler, name);
-  dlclose(handler);
 
-  // Arguments initialization
-  values[0] = &arg1;
-  args[0] = &ffi_type_sint;
-  values[1] = &arg2;
-  args[1] = &ffi_type_pointer;
-  values[2] = &arg3;
-  args[2] = &bar_type;
+  int params[50];
 
-  // Calling :)
-  if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 3, &ffi_type_sint, args) == FFI_OK)
-    {
-      arg1 = 21;
-      arg2 = "Uh, Oh";
-      arg3.x = 14;
-      arg3.y = 88;
+  for (i=0; i < count; i++) {
+    params[i] = (int)POP_DATA_STACK;
+    values[i] = &params[i];
+    args[i] = &ffi_type_sint;
+  }
 
-      ffi_call(&cif, ptr, &res, values);
+  if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, count, &ffi_type_sint, args) == FFI_OK) {
+    ffi_call(&cif, ptr, &res, values);
+  }
 
-      printf("And the dead beef is %d\n", res);
-    }
-*/
-
-static cell_t CCall(cell_t count) {
-    MSG_NUM_D("Count = ", count);
-    return count + 1;
+  return res;
 }
 
 #ifdef PF_NO_GLOBAL_INIT
@@ -90,7 +73,14 @@ CFunc0 CustomFunctionTable[] ={
 Err CompileCustomFunctions(void) {
     Err err;
     int i = 0;
-    err = CreateGlueToC("CCALL", i++, C_RETURNS_VALUE, 1);
+    handler = dlopen(NULL, RTLD_LAZY);
+    if (!handler)
+    {
+      fprintf(stderr, "dlopen failed: %s\n", dlerror());
+      exit(EXIT_FAILURE);
+    }
+    /* TODO: We also need dclose(handler) somewhere. */
+    err = CreateGlueToC("CCALL", i++, C_RETURNS_VALUE, 2);
     if( err < 0 ) return err;
 
     return 0;
